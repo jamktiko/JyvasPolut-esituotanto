8f6934b3cced9a678be6f21298597e8e4e6a478d
Jami, Salonen <Jami.Salonen@domain.fi>
2026-04-24 13:34:48 +0300

    npm install tehty

--- JyvasPolut/src/routes/+layout.svelte
+++ JyvasPolut/src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { appi } from '$lib/appInfo';
 	import Header from '$lib/Header.svelte';
 	import Footer from '$lib/Footer.svelte';
+
 	let { children } = $props();
 </script>
 
@@ -10,16 +11,21 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<Header text={appi.name} />
-<main class="content">
-	{@render children()}
-</main>
-<Footer text={appi.cr} />
+<div class="min-h-screen bg-gradient-to-r from-[#0F172A] to-[#022C22]">
+	<Header text={appi.name} />
+
+	<main class="content">
+		{@render children()}
+	</main>
+
+	<Footer text={appi.cr} />
+</div>
 
 <style>
 	.content {
-		padding-bottom: 2.5em; /* footerin korkeus */
+		padding-bottom: 2.5em;
 	}
+
 	:global(html, body) {
 		margin: 0;
 		padding: 0;
